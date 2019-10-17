6672c3fe36f0d4ddf26d77f95745e6a4c47aa4e2
Kate Laney <kate.a.laney@gmail.com>
2019-10-17 16:19:52 -0400

    working on Word constructor

--- word.js
+++ word.js
@@ -3,11 +3,12 @@ var Letter = require("./letter.js");
 
 // Create a "Word" constructor that depends on the Letter constructor.
 
-var Word = {
-    
+var Word = function() {
+    this.letterArr = []; // An array of new Letter objects representing the letters of the underlying word.
+    this.
 }
 
-// An array of new Letter objects representing the letters of the underlying word
+
 // A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
 // A function that takes a character as an argument and calls the guess function on each letter object (the second function defined in Letter.js)
 
